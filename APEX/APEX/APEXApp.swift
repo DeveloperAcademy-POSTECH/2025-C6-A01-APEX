6f17a3008f2371ec6f53be501ab8f6463a6a1e53
UnkyungJo <pos10022@unist.ac.kr>
2025-09-20 20:30:40 +0900

    chore: 프로젝트 생성

---
+++ APEX/APEX/APEXApp.swift
@@ -0,0 +1,17 @@
+//
+//  APEXApp.swift
+//  APEX
+//
+//  Created by 조운경 on 9/20/25.
+//
+
+import SwiftUI
+
+@main
+struct APEXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APEX/APEXTests/APEXTests.swift
@@ -0,0 +1,17 @@
+//
+//  APEXTests.swift
+//  APEXTests
+//
+//  Created by 조운경 on 9/20/25.
+//
+
+import Testing
+@testable import APEX
+
+struct APEXTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
