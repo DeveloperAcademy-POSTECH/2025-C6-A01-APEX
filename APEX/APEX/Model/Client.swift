4443c2f3241d891fe7406f95a03b799c75c8c84f
UnkyungJo <pos10022@unist.ac.kr>
2025-10-08 15:24:49 +0900

    feat: Client 모델 추가

--- APEX/APEX/Model/Client.swift
+++ APEX/APEX/Model/Client.swift
@@ -4,7 +4,19 @@
 //
 //  Created by 조운경 on 10/7/25.
 //
+import Foundation
+import SwiftUI
 
 struct client {
-    
+    let profile: Image // 샘플데이터용 타입
+    let nameCard: Image // 샘플데이터용 타입
+    let surname: String
+    let name: String
+    let position: String
+    let company: String
+    let email: String
+    let phoneNumber: String
+    let linkedinURL: String
+    let memo: String
+    let action: String
 }
