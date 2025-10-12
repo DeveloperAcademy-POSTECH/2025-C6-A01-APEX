1b50496a695852304608ba0b1d759b5beecc2a61
UnkyungJo <pos10022@unist.ac.kr>
2025-10-12 20:59:31 +0900

    chore: 폰트 추가

---
+++ APEX/APEX/Util/Extension/Font+Ex.swift
@@ -0,0 +1,14 @@
+//
+//  Font+Ex.swift
+//  APEX
+//
+//  Created by 조운경 on 10/10/25.
+//
+
+import SwiftUI
+
+extension Font {
+    static let body1 = Font.system(size: 16).weight(.semibold)
+    static let body2 = Font.system(size: 16).weight(.medium)
+    static let body6 = Font.system(size: 14).weight(.regular)
+}
