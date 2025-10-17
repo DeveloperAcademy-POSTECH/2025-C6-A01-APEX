4b24f5e0e22114fe4b6f42d52a52be2cc32d3969
UnkyungJo <pos10022@unist.ac.kr>
2025-10-18 03:11:14 +0900

    feat: 탭바 추가

---
+++ APEX/APEX/Presentation/Root/RootView.swift
@@ -0,0 +1,37 @@
+//
+//  RootView.swift
+//  APEX
+//
+//  Created by 조운경 on 10/18/25.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    enum Tabs { case contacts, notes, search }
+    
+    @State private var selection: Tabs = .contacts
+    
+    @State private var contactsQuery: String = ""
+    @State private var notesQuery: String = ""
+    
+    var body: some View {
+        TabView(selection: $selection) {
+            Tab("Contacts", systemImage: "person.3.fill", value: .contacts) {
+                ContactsView()
+            }
+            
+            Tab("Notes", systemImage: "note.text", value: .search) {
+                NotesView()
+            }
+            
+            Tab(value: .search, role: .search) {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    RootView()
+}
