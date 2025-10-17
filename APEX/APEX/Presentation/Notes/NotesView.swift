ec74eac2820299eadbe6d08f76d7044e1eea9455
UnkyungJo <pos10022@unist.ac.kr>
2025-10-18 03:10:42 +0900

    chore: ChattingOutSideView를 NotesView로 이름 변경

--- APEX/APEX/Presentation/Notes/NotesView.swift
+++ APEX/APEX/Presentation/Notes/NotesView.swift
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct ChattingOutsideView: View {
+struct NotesView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ChattingOutsideView()
+    NotesView()
 }
